1c74e2194ed5068c6d5340baabc6c7f4011a13aa
“ddevitz” <danieladevitz@gmail.com>
2024-08-10 20:09:42 -0300

    travel page modal created

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,6 +1,17 @@
-import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog } from "lucide-react";
+import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, Tag, UserCog, X } from "lucide-react";
+import { useState } from "react";
 
 export function TripDetailsPage(){
+    const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false)
+
+    function openCreateActivityModal(){
+        setIsCreateActivityModalOpen(true)
+    }
+
+    function closeCreateActivityModal(){
+        setIsCreateActivityModalOpen(false)
+    }
+
     return(
         <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
             <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
@@ -28,7 +39,7 @@ export function TripDetailsPage(){
                 <div className="flex-1 space-y-6">
                     <div className="flex items-center justify-between">
                         <h2 className="text-3xl font-semibold">Atividades</h2>
-                        <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                        <button onClick={openCreateActivityModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
                             <Plus className="size-5"/>
                             Cadastrar atividade
                         </button>
@@ -102,37 +113,82 @@ export function TripDetailsPage(){
                     <div className="w-full h-px bg-zinc-800"></div>
 
                     <div>
-                    <div className="space-y-6">
-                        <h2 className="font-semibold text-xl">Convidados</h2>
+                        <div className="space-y-6">
+                            <h2 className="font-semibold text-xl">Convidados</h2>
 
-                        <div className="space-y-5">
-                            <div className="flex items-center justify-between gap-4">
-                                <div className="space-y-1.5">
-                                    <span className="block font-medium text-zinc-100">Daniela Devitz</span>
-                                    <span className="block text-sm text-zinc-400 truncate">danieladevitz@gmail.com</span>
+                            <div className="space-y-5">
+                                <div className="flex items-center justify-between gap-4">
+                                    <div className="space-y-1.5">
+                                        <span className="block font-medium text-zinc-100">Daniela Devitz</span>
+                                        <span className="block text-sm text-zinc-400 truncate">danieladevitz@gmail.com</span>
+                                    </div>
+                                    <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
                                 </div>
-                                <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
                             </div>
-                        </div>
 
-                        <div className="space-y-5">
-                            <div className="flex items-center justify-between gap-4">
-                                <div className="space-y-1.5">
-                                    <span className="block font-medium text-zinc-100">Antonio Renato</span>
-                                    <span className="block text-sm text-zinc-400 truncate ">antoniorenato@gmail.com</span>
+                            <div className="space-y-5">
+                                <div className="flex items-center justify-between gap-4">
+                                    <div className="space-y-1.5">
+                                        <span className="block font-medium text-zinc-100">Antonio Renato</span>
+                                        <span className="block text-sm text-zinc-400 truncate ">antoniorenato@gmail.com</span>
+                                    </div>
+                                    <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
                                 </div>
-                                <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
                             </div>
-                        </div>
 
-                        <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700"> 
-                            <UserCog className="size-5"/>
-                            Gerenciar convidados
-                    </button>
-                    </div>
+                            <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700"> 
+                                <UserCog className="size-5"/>
+                                Gerenciar convidados
+                            </button>
+                        </div>
                     </div>
                 </div>
             </main>
+
+            {isCreateActivityModalOpen && (
+                <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+                    <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+                        <div className="space-y-2">
+                            <div className="flex items-center justify-between">
+                            <h2 className="text-lg font-semibold">Cadastrar atividade</h2>
+                            <button type="button" onClick={closeCreateActivityModal}>
+                                <X className="size-5 text-zinc-400"/>
+                            </button>
+                            </div>
+                
+                            <p className="text-sm text-zinc-400">
+                                Todos os convidados podem visualizar as atividades.
+                            </p>
+                        </div>
+            
+                        <form className="space-y-3">
+                            <div className="h-14 px-4 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
+                                <Tag className="text-zinc-400 size-5"/>
+                                <input 
+                                    name="title" 
+                                    placeholder="Qual a atividade" 
+                                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                                />
+                            </div>
+                
+                            <div className="flex items-center gap-2">
+                                <div className="h-14 flex-1 px-4 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
+                                    <input 
+                                        type="datetime-local" 
+                                        name="occurs_at" 
+                                        placeholder="Data e horário da atividade" 
+                                        className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                                    />
+                                </div>
+                            </div>
+                
+                            <button type="submit" className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
+                                Salvar atividade
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
